4b839e756675f454a0644bb58b1f8073fe21722c
dwesh163 <dwesh163@protonmail.com>
2026-09-06 22:57:31 +0200

    [feature] Add Kooked project and update existing project links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,13 +5,18 @@ export type Project = {
 };
 
 export const projects: Project[] = [
+  { id: "kooked", title: "Kooked", link: "https://kooked.ch" },
   { id: "music", title: "Kooked Music", link: "https://musics.kooked.ch" },
   { id: "pictures", title: "Kooked Pictures", link: "https://pictures.kooked.ch" },
-  { id: "grafana", title: "Grafana", link: "https://grafana.kooked.ch" },
   { id: "md2pdf", title: "Markdown to PDF", link: "https://md2pdf.kooked.app" },
-  { id: "password-game", title: "Password Game", link: "https://password-game.kooked.app" },
-  { id: "uptime-kuma", title: "Uptime Kuma", link: "https://kuma.kooked.app" },
   { id: "url-shortener", title: "URL Shortener", link: "https://go.kooked.app" },
-  { id: "api-ict", title: "API ICT", link: "https://api-ict.kooked.app" },
+  { id: "api-ict", title: "API ICT", link: "https://api.kooked.app/ict" },
+  { id: "balladur", title: "Balladur", link: "https://balladur.kooked.app" },
+  { id: "ctfd", title: "CTFd", link: "https://ctfd.kooked.ch" },
+  { id: "n8n", title: "N8N", link: "https://n8n.kooked.ch" },
+  { id: "node-red", title: "Node-RED", link: "https://nodered.kooked.ch" },
+  { id: "projects", title: "OpenProjects", link: "https://projects.kooked.ch" },
+  { id: "rahoot", title: "Rahoot", link: "https://rahoot.kooked.ch" },
+  { id: "odoo", title: "Odoo", link: "https://odoo.kooked.ch" },
   { id: "moodle", title: "Kooked Moodle", link: "https://moodle.kooked.ch" },
 ];
